ff061177e616cfcf193b6d8ef72e4323faf19e71
Thomas Schwartz <thomasschwartz@thomass-mbp-2.lan>
2025-07-04 15:10:38 -0400

    Set Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/thomasschwartz18.github.io/Historle-React/',
+  base: '/Historle-React/',
   plugins: [react()],
 })
